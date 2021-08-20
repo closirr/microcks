f1512f189c0194267f1140580ba1d5f3455e9308
closirr <closirr@gmail.com>
2021-08-20 10:57:43 +0300

    Update setup.sh

--- install/docker-compose/setup.sh
+++ install/docker-compose/setup.sh
@@ -20,8 +20,8 @@ cp keycloak-realm/microcks-realm-sample.json keycloak-realm/microcks-realm-sampl
 echo "Replacing hostname in configuration files ..."
 jq '.applications |= map(if .name == "microcks-app-js" then .redirectUris = [ "https://'${HOSTNAME}':8080/*" ] else . end)' keycloak-realm/microcks-realm-sample.json.bak > keycloak-realm/microcks-realm-sample.json
 
-perl -i.bak -pe 's|- KEYCLOAK_URL=https://docker.for.mac.localhost:8543/auth|#- KEYCLOAK_URL=https://docker.for.mac.localhost:8543/auth|' microcks.yml
-perl -i.bak -pe 's|#- KEYCLOAK_URL=https://localhost:8180/auth|- KEYCLOAK_URL=http://'${HOSTNAME}':8543/auth|' microcks.yml
+perl -i.bak -pe 's|- KEYCLOAK_URL=https://docker.for.mac.localhost:8543/auth|#- KEYCLOAK_URL=https://docker.for.mac.localhost:8543/auth|' docker-compose.yml
+perl -i.bak -pe 's|#- KEYCLOAK_URL=https://localhost:8180/auth|- KEYCLOAK_URL=http://'${HOSTNAME}':8543/auth|' docker-compose.yml
 
 echo "Generating certificates for '$HOSTNAME' ..."
 mkdir keystore
@@ -32,17 +32,17 @@ mv ./keystore/server.p12 ./keystore/microcks.p12
 
 if [ ${VERSION} != "latest" ]; then
   echo "Replacing version in configuration files ..."
-  perl -i.bak -pe 's|microcks/microcks:latest|microcks/microcks:${VERSION}|' microcks.yml
+  perl -i.bak -pe 's|microcks/microcks:latest|microcks/microcks:${VERSION}|' docker-compose.yml
 fi
 
 echo
 echo "Microcks is now installed with self-signed certificates"
 echo "------------------------------------------"
-echo "Start it with: docker-compose -f microcks.yml up -d"
-echo "Stop it with: docker-compose -f microcks.yml stop"
-echo "Re-launch it with: docker-compose -f microcks.yml start"
-echo "Clean everything with: docker-compose -f microcks.yml down"
+echo "Start it with: docker-compose -f docker-compose.yml up -d"
+echo "Stop it with: docker-compose -f docker-compose.yml stop"
+echo "Re-launch it with: docker-compose -f docker-compose.yml start"
+echo "Clean everything with: docker-compose -f docker-compose.yml down"
 echo "------------------------------------------"
 echo "Go to https://$HOSTNAME:8080 - first login with admin/123"
-echo "Having issues? Check you have changed microcks.yml to your platform"
+echo "Having issues? Check you have changed docker-compose.yml to your platform"
 echo
